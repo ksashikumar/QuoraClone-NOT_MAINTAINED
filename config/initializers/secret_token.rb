5fce31360ea39959f38cc40db74fceaf97dbf9f5
ksashikumar <ksashikumark93@gmail.com>
2016-02-18 12:40:15 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuoraClone::Application.config.secret_token = 'b3cddffcaeb3648f10c057f5a9f4770ff50a74a09f6a688c43e1824bcaaa64ae0155001080c955ca36c1111df8dc4ad526bfcfe6ffd4f73f623a9783207ef5aa'
