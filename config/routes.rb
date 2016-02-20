8f477e70c06947051aa1c9c9f59e61b9b8f2b9cd
ksashikumar <ksashikumark93@gmail.com>
2016-02-21 01:19:46 +0530

    .gitignore is now working

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-QuoraClone::Application.routes.draw do
-  get "signup" => "users#new", :as => "signup"
-  get "signin" => "sessions#new", :as => "signin"
-  get "signout" => "sessions#destroy", :as => "signout"
-  root :to => "home#home"
-  resources :users
-  resources :sessions
-end
